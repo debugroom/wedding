22b48be2ac5081c8721b385044b88dd784a1c2bd
kawabatakouhei <kohei.kawabata@mac.com>
2015-09-03 22:12:29 +0900

    Add UserRepositoryTest skelton class.

---
+++ wedding-infra-jpa/src/test/java/org/debugroom/wedding/domain/repository/basic/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package org.debugroom.wedding.domain.repository.basic;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.TestContextManager;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * 
+ * @author org.debugroom
+ *
+ */
+@RunWith(Enclosed.class)
+public class UserRepositoryTest {
+
+}
