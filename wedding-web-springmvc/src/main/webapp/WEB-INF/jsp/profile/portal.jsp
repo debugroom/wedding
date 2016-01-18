ed452b787117e3bee9a75d3015b137fcb450a4d4
kohei.kawabata <kohei.kawabata@mac.com>
2016-01-19 00:59:01 +0900

    Modify javascript for profile modal screen.

--- wedding-web-springmvc/src/main/webapp/WEB-INF/jsp/profile/portal.jsp
+++ wedding-web-springmvc/src/main/webapp/WEB-INF/jsp/profile/portal.jsp
@@ -24,6 +24,7 @@
     	<article>
     		<div class="panel">
     			<div class="formPanel">
+    				変更したい項目がある場合は、各項目の変更ボタンを押して、変更内容を入力して更新したのち、最後に一番下の更新ボタンを押してください。
     				<form:form action="${pageContext.request.contextPath}/profile/edit"
 						modelAttribute="user">
 						<form:hidden path="userId"/>
@@ -32,37 +33,38 @@
 								<tr>
 									<td><form:label path="userName">ユーザ名</form:label> : </td>
 									<td>${user.userName}<form:hidden path="userName"/></td>
-									<td><form:button name="updateParam" value="userName" >更新</form:button></td>
+									<td><button id="userName-button" name="userName-button" type="button" value="userName" >変更</button></td>
 								</tr>
 								<tr>
 									<td><form:label path="imageFilePath">ピクチャ</form:label> : </td>
 									<td><img src="${pageContext.request.contextPath}${user.imageFilePath}"><form:hidden path="imageFilePath"/></td>
-									<td><form:button name="updateParam" value="imageFile" >更新</form:button></td>
+									<td><button id="imageFile-button" name="imageFile-button" type="button" value="imageFile" >変更</button></td>
 								</tr>
 								<tr>
 									<td><form:label path="loginId">ログインID</form:label> : </td>
 									<td>${user.loginId}<form:hidden path="loginId"/></td>
-									<td><form:button name="updateParam" value="loginId" >更新</form:button></td>
+									<td><button id="loginId-button" name="loginId-button" type="button" value="loginId" >変更</button></td>
 								</tr>
 								<tr>
 									<td><form:label path="address.postCd">郵便番号</form:label> : </td>
 									<td>${user.address.postCd}<form:hidden path="address.postCd"/></td>
-									<td><form:button name="updateParam" value="address.postCd" >更新</form:button></td>
+									<td><button id="address.postCd-button" name="address.postCd-button" type="button" value="address.postCd" >変更</button></td>
 								</tr>
 								<tr>
 									<td><form:label path="address.address">住所</form:label> : </td>
 									<td>${user.address.address}<form:hidden path="address.address"/></td>
-									<td><form:button name="updateParam" value="address.address" >更新</form:button></td>
+									<td><button id="address.address-button" name="address.address-button" type="button" value="address.address" >変更</button></td>
 								</tr>
     	   						<c:forEach items="${user.emails}" var="email" varStatus="status">
 								<tr>
 									<td><label for="emails[${status.index}].email">Email</label>${status.index+1} : </td>
 									<td>${email.email}<input id="emails[${status.index}].email" name="emails[${status.index}].email" type="hidden" value="${email.email}">
-									<td><form:button name="updateParam" value="email_${status.index}" >更新</form:button></td>
+									<td><button id="emails[${status.index}].email-button" name="emails[${status.index}].email-button" type="button" value="emails[${status.index}].email" >変更</button></td>
 								</tr>
     							</c:forEach>
 							</tbody>
 						</table>
+						<form:button class="main-button" name="updateParam" >更新を確定する</form:button>
 					</form:form>
     			</div>
     	   </div>
--- wedding-web-springmvc/src/main/webapp/resources/app/js/profile.js
+++ wedding-web-springmvc/src/main/webapp/resources/app/js/profile.js
@@ -8,18 +8,118 @@ document.addEventListener('DOMContentLoaded', function(){
 	
 	//for in文の方が書きやすいが、パフォーマンス問題と、キー・値の混同が生じやすいので使わない。
 	for(var i=0 ; i < buttonArray.length ; i++ ){
-		//各ボタンにリスナーを設定。
-		buttonArray[i].addEventListener("click", function(){showDialog(formElement, 
-				buttonArray[i].id, buttonArray[i].value)}, false);
+		//各ボタンにリスナーを設定。イベント発火タイミングでリスナーに設定された関数が評価されるため、
+		//パラメータに変数を指定する場合は注意。パラメータは定数値を設定しておくか、Event経由で取得した方が確実。
+		buttonArray[i].addEventListener("click", function(event){
+			showUpdateDialog(event);
+		}, false);
 	}
-	
-	
 }, false)
 
-function showDialog(formElement, id, value){
-	alert(value);
-	alert(value);
-	var inputElement = formElement.getElementById(button.value);
-	alert(inputElement.value);
-	var form = document.createElement("div");
+function showUpdateDialog(event){
+	//UPDATE対象のパラメータを取得する。
+	var value = event.currentTarget.value;
+	//配列を含む場合のパラメータ表現を変更する。
+	var target = replaceArrayExpression(value);
+
+	//パラメータを変更するためのパネルを作る。
+	var panelElement = document.createElement("div");
+	//パラメータを変更するためのパネルの属性をmodelPanelとする。
+	panelElement.setAttribute("class", "editPanel");
+	//インプットフォームを作成する。
+	var inputElement = document.createElement("input");
+	//パラメータによってフォームの種類を変更する。
+	switch (target)	{
+		case "userName" : 
+			inputElement.setAttribute("type", "text");
+			break;
+		case "imageFile" : 
+			inputElement.setAttribute("type", "text");
+			break;
+		case "loginId" : 
+			inputElement.setAttribute("type", "text");
+			break;
+		case "address.postCd" : 
+			inputElement.setAttribute("type", "text");
+			break;
+		case "address.address" : 
+			inputElement.setAttribute("type", "text");
+			break;
+		case "emails" : 
+			inputElement.setAttribute("type", "email");
+			break;
+	}
+	//現在入力されているパラメータをhiddenから取得してフォームに設定する。
+	inputElement.setAttribute("value", document.getElementById(value).value);
+	panelElement.appendChild(inputElement);
+
+	//キャンセルボタンを作成する。
+	var cancelButtonElement = document.createElement("button");
+	var cancelButtonTitle = document.createTextNode("閉じる"); 
+	cancelButtonElement.setAttribute("id", "close");
+	cancelButtonElement.addEventListener("click", function(event){
+		closeUpdateDialog(event);},
+		false
+	);
+	cancelButtonElement.appendChild(cancelButtonTitle);
+	panelElement.appendChild(cancelButtonElement);
+
+	//更新ボタンを作成する。
+	var submitButtonElement = document.createElement("button");
+	var submitButtonTitle = document.createTextNode("変更"); 
+	submitButtonElement.setAttribute("id", "updateParam");
+	submitButtonElement.setAttribute("type", "button");
+	submitButtonElement.addEventListener("click", function(event){
+		updateParam(event);},
+		false
+	);
+	submitButtonElement.appendChild(submitButtonTitle);
+	panelElement.appendChild(submitButtonElement);
+	
+	document.getElementById(value).parentNode.appendChild(panelElement);
+
+}
+
+function closeUpdateDialog(event){
+	event.currentTarget.parentNode.parentNode.removeChild(
+			event.currentTarget.parentNode);
+}
+
+function updateParam(event){
+	//表示内容の変更
+	//<td>
+	//"表示項目"
+	//	<input type="hidden" value="hiddenParam">
+	//  <div class="panel">
+	//    <input type="form" value="inputContents">
+	//    <button type="button"> <--event.currentTarget
+	event.currentTarget.parentNode.parentNode.firstChild.textContent = 
+		event.currentTarget.parentNode.firstElementChild.value;
+	//警告文を作成する。
+	var test = document.getElementById(event.currentTarget.name + "-message");
+	if(null == document.getElementById(event.currentTarget.name + "-message")){
+		var warningFragment = document.createDocumentFragment();
+		var warningPanel = document.createElement("div");
+		warningPanel.setAttribute("id", event.currentTarget.name + "-message");
+		warningPanel.setAttribute("class", "warningMessage");
+		warningPanel.appendChild(document.createTextNode("!下のボタンを押して、更新を確定してください。"));
+		warningFragment.appendChild(warningPanel);
+		event.currentTarget.parentNode.parentNode.appendChild(warningFragment); 
+	}
+
+	//Hidden項目の変更
+	event.currentTarget.parentNode.parentNode.firstElementChild.value = 
+		event.currentTarget.parentNode.firstElementChild.value;
+	//編集パネルを削除
+	event.currentTarget.parentNode.parentNode.removeChild(
+			event.currentTarget.parentNode);
+}
+
+function replaceArrayExpression(value){
+	var regExp = new RegExp("[0-9a-zA-Z¥.]+[¥[].*");
+	if(regExp.test(value)){
+		var splitString = value.slice(0, value.search("[¥[]"));
+		return splitString;
+	}
+	return value;
 }
