c5d93582d3d03cffb54cefdc38a23fa85fe8d1b4
kawabatakouhei <kohei.kawabata@mac.com>
2017-08-14 16:19:15 +0900

    Modify directory traversal issue.

--- wedding-microservice/wedding-batch-gallery/src/main/java/org/debugroom/wedding/app/web/gallery/GalleryController.java
+++ wedding-microservice/wedding-batch-gallery/src/main/java/org/debugroom/wedding/app/web/gallery/GalleryController.java
@@ -1,12 +1,12 @@
 package org.debugroom.wedding.app.web.gallery;
 
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 
+import org.debugroom.framework.common.exception.BusinessException;
 import org.debugroom.wedding.app.web.gallery.helper.DownloadHelper;
+import org.debugroom.wedding.app.web.gallery.model.Credential;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -32,10 +35,15 @@ public class GalleryController {
 	@CrossOrigin
 	@RequestMapping(value="/gallery/archive", method=RequestMethod.GET,
 			params="accessKey")
-	public HttpEntity<InputStreamResource> getArchive(@RequestParam String accessKey) 
-			throws FileNotFoundException{
+	public HttpEntity<InputStreamResource> getArchive(
+			@Valid Credential credential, BindingResult bindingResult) 
+			throws BusinessException, IOException{
 		
-		File file = downloadHelper.getDownloadFile(accessKey);
+		if(bindingResult.hasErrors()){
+			return ResponseEntity.badRequest().body(null);
+		}
+
+		File file = downloadHelper.getDownloadFile(credential.getAccessKey());
 
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
--- wedding-microservice/wedding-batch-gallery/src/main/java/org/debugroom/wedding/app/web/gallery/helper/DownloadHelper.java
+++ wedding-microservice/wedding-batch-gallery/src/main/java/org/debugroom/wedding/app/web/gallery/helper/DownloadHelper.java
@@ -1,9 +1,12 @@
 package org.debugroom.wedding.app.web.gallery.helper;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
+import org.debugroom.framework.common.exception.BusinessException;
 import org.debugroom.wedding.app.batch.gallery.GalleryBatchProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +16,9 @@ public class DownloadHelper {
 	@Inject
 	GalleryBatchProperties galleryBatchProperties;
 	
-	public File getDownloadFile(String accessKey){
-		return new File(new StringBuilder()
+	public File getDownloadFile(String accessKey) 
+			throws BusinessException, IOException{
+		File downloadFile = new File(new StringBuilder()
 				.append(galleryBatchProperties.getGalleryDownloadWorkRootDirectory())
 				.append(java.io.File.separator)
 				.append(galleryBatchProperties.getGalleryDownloadDirectory())
@@ -23,5 +27,13 @@ public class DownloadHelper {
 				.append(java.io.File.separator)
 				.append(galleryBatchProperties.getGalleryDownloadFilename())
 				.toString());
+		
+		// ディレクトリトラバーサル対策
+		if(!StringUtils.startsWith(downloadFile.getCanonicalPath(), 
+				galleryBatchProperties.getGalleryDownloadWorkRootDirectory())){
+			throw new BusinessException("downloadHelper.error.0001", null, 
+					downloadFile.getCanonicalFile());
+		}
+		return downloadFile;
 	}
 }
