f0d204819c7b9c9a1dd84ca1906510901aefd775
root <root@ip-172-31-21-49.ap-northeast-1.compute.internal>
2017-11-03 17:47:44 +0000

    Fix bug on dockerfile.

--- build-production-servers/build-postgresql/scripts/init_db.sh
+++ build-production-servers/build-postgresql/scripts/init_db.sh
@@ -2,8 +2,8 @@
 
 set PASSWORD=postgres
 
-su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/create_role.sql;pg_ctl stop -m fast"
-su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/create_db.sql;pg_ctl stop -m fast"
-su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/create_table.sql -d wedding;pg_ctl stop -m fast"
-su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/grant_role.sql -d wedding;pg_ctl stop -m fast"
-su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/init_data.sql -d wedding;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/wedding/build-production-servers/build-postgresql/scripts/create_role.sql;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/wedding/build-production-servers/build-postgresql/scripts/create_db.sql;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/wedding/build-production-servers/build-postgresql/scripts/create_table.sql -d wedding;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/wedding/build-production-servers/build-postgresql/scripts/grant_role.sql -d wedding;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/wedding/build-production-servers/build-postgresql/scripts/init_data.sql -d wedding;pg_ctl stop -m fast"
