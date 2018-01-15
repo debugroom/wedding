29f9913225c77f5036c576a41a7f1f0e98ae03e4
kawabatakouhei <kohei.kawabata@mac.com>
2018-01-15 15:23:07 +0900

    Fix bug for production environments.

--- wedding-microservice/wedding-batch-common/src/main/java/org/debugroom/wedding/config/infra/BatchInfraProductionConfig.java
+++ wedding-microservice/wedding-batch-common/src/main/java/org/debugroom/wedding/config/infra/BatchInfraProductionConfig.java
@@ -2,8 +2,11 @@ package org.debugroom.wedding.config.infra;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -13,8 +16,26 @@ import lombok.Data;
 @Data
 @Configuration
 @Profile("production")
+@ConfigurationProperties(prefix="spring.datasource")
 public class BatchInfraProductionConfig {
 
+	private String url;
+	private String username;
+	private String password;
+	private String driverClassName;
+
+	@Bean
+	@Primary
+	public DataSource dataSource(){
+	    return DataSourceBuilder
+	    		.create()
+	            .driverClassName(driverClassName)
+	            .url(url)
+	            .username(username)
+	            .password(password)
+	            .build();	
+	}
+	
 	@Bean(name="batchDataSource")
 	public DataSource jobRepositoryEmbeddedDataSource(){
 		return new EmbeddedDatabaseBuilder()
