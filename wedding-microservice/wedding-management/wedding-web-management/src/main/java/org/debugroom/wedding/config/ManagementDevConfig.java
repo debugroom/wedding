fe5b6bddcb50ae5d8aa8dec9d08f4ef260f89b5e
kawabatakouhei <kohei.kawabata@mac.com>
2018-01-16 12:12:11 +0900

    Fix bug for production environments.

---
+++ wedding-microservice/wedding-management/wedding-web-management/src/main/java/org/debugroom/wedding/config/ManagementDevConfig.java
@@ -0,0 +1,11 @@
+package org.debugroom.wedding.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Profile("dev")
+@PropertySource(value = {"classpath:/docker-service-dev.properties"})
+@Configuration
+public class ManagementDevConfig {
+}
